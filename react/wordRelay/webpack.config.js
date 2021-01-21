cbdc9556367d7a6ddfd6f36adec3595d526a7798
kimnamsun <nsunny0908@naver.com>
2021-01-22 01:36:54 +0900

    wordRelay webpack, babel setting

--- react/wordRelay/webpack.config.js
+++ react/wordRelay/webpack.config.js
@@ -1,3 +1,28 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', //실서비스 : production
+    devtool: 'eval',
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    entry: {
+        app: ['./client'], //다른파일에서 이미 불러오고 있는 파일은 적어줄 필요 없음.
+    }, //입력
+
+    module: {
+        rules: [{
+            test: /\.jsx?/,
+            loader: 'babel-loader',
+            options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+                plugins: ['@babel/plugin-proposal-class-properties'],
+            },
+        }],
+    },
+    output: {
+        path: path.join(__dirname, 'dist'), //경로를 알아서 합쳐줌
+        filename: 'app.js'
+    }, //출력
 };
