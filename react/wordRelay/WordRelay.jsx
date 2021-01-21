08274ea74feeeece758b8a5f6ec6793fd2a25f66
kimnamsun <nsunny0908@naver.com>
2021-01-22 03:38:25 +0900

    devserver, hot reloading setting

--- react/wordRelay/WordRelay.jsx
+++ react/wordRelay/WordRelay.jsx
@@ -3,11 +3,50 @@ const { Component } = React;
 
 class WordRelay extends Component {
     state = {
-        text: 'Hello, webpack',
+        word: '리액트',
+        value: '',
+        result: '',
+    };
+
+    onSubmitForm = (e) => {
+        e.preventDefault();
+        //끝말잇기 로직이 들어가는 부분
+        if (this.state.word[this.state.word.length - 1] === this.state.value[0]) {
+            this.setState({
+                result: '딩동댕🥰',
+                word: this.state.value,
+                value: '',
+            })
+        } else {
+            this.setState({
+                result: '땡😢',
+                value: '',
+            })
+        }
+        this.input.focus();
+    };
+
+    onChangeInput = (e) => {
+        this.setState({ value: e.target.value });
+    };
+
+    input;
+
+    onRefInput = (c) => {
+        this.input = c;
     };
 
     render() {
-        return <h1>{this.state.text}</h1>
+        return (
+            <>
+                <h3>{this.state.word}</h3>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput} />
+                    <button>입력!</button>
+                </form>
+                <h3>{this.state.result}</h3>
+            </>
+        );
     }
 }
 
--- react/wordRelay/webpack.config.js
+++ react/wordRelay/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
 
 module.exports = {
     name: 'wordrelay-setting',
@@ -16,13 +17,32 @@ module.exports = {
             test: /\.jsx?/,
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env', '@babel/preset-react'],
-                plugins: ['@babel/plugin-proposal-class-properties'],
+                presets: [
+                    ['@babel/preset-env', {
+                        targets: {
+                            browsers: ['> 1% in KR'],
+                        },
+                        debug: true,
+                    }],
+                    '@babel/preset-react',
+                ],
+                plugins: [
+                    '@babel/plugin-proposal-class-properties',
+                    'react-refresh/babel',
+                ],
             },
         }],
     },
+    plugins: [
+        new RefreshWebpackPlugin()
+    ],
     output: {
         path: path.join(__dirname, 'dist'), //경로를 알아서 합쳐줌
-        filename: 'app.js'
+        filename: 'app.js',
+        publicPath: '/dist/', //webpack-dev-server가 사용하는 결과물간의 상대 경로 
     }, //출력
+    devServer: {
+        publicPath: '/dist/',
+        hot: true,
+    },
 };
