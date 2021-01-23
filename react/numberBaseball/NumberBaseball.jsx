39486c57d5a9c67ac3e919a09e65e94b0f00ce9e
kimnamsun <nsunny0908@naver.com>
2021-01-23 16:38:46 +0900

    0123 commit

--- react/numberBaseball/NumberBaseball.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-class NumberBaseball extends Component {
-
-}
-
-export const hello = 'hello'; //import {hello}
-export const bye = 'bye'; // import {hello, bye}
-
-export default NumberBaseball; //import NumberBaseball;
