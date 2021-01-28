a47001b86be6024dfd2030967c371aa52e01864c
kimnamsun <nsunny0908@naver.com>
2021-01-29 05:21:53 +0900

    0129 commit

---
+++ react/ticTacToe/Td.jsx
@@ -0,0 +1,19 @@
+import React, { useCallback } from 'react';
+import { CLICK_CELL } from './TicTacToe';
+
+const Td = ({ rowIndex, cellIndex, dispatch, cellData }) => {
+    const onClickTd = useCallback (() => {
+        console.log(rowIndex, cellIndex);
+        //기존에 셀데이터가 있으면
+        if (cellData) {
+            return;
+        }
+        dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex });
+    }, [cellData]);
+
+    return(
+        <td onClick={onClickTd}>{cellData}</td>
+    )
+}
+
+export default Td;
